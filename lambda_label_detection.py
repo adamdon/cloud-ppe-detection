c52e44d6b7104cb78acf13d6c5160a5d04d2427f
adamdon <mail@adamdon.co.uk>
2022-03-27 19:38:34 +0000

    Event, record, message, image parsing added to lamda

--- lambda_label_detection.py
+++ lambda_label_detection.py
@@ -4,7 +4,16 @@ import boto3
 
 def lambda_handler(event, context):
     print("lambda_label_detection.py running...")
-    print(event)
+
+    body = json.loads(event["Records"][0]["body"]) 
+    message = json.loads(body["Message"])
+    
+    bucketName = message["Records"][0]["s3"]["bucket"]["name"]
+    imageName = message["Records"][0]["s3"]["object"]["key"]
+    print(bucketName)
+    print(imageName)
+    
+    print("... End of lambda_label_detection.py")
 
     # rekognitionClient = boto3.client("rekognition")
 
