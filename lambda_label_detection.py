35cb040994031dfe6de7f3df782fe539e353f9aa
EC2 Default User <ec2-user@ip-172-31-40-177.ec2.internal>
2022-03-28 18:27:05 +0000

    Added rekognition lable detection

--- lambda_label_detection.py
+++ lambda_label_detection.py
@@ -10,11 +10,25 @@ def lambda_handler(event, context):
     
     bucketName = message["Records"][0]["s3"]["bucket"]["name"]
     imageName = message["Records"][0]["s3"]["object"]["key"]
+    tableName = ("table" + bucketName[2:])
+    
     print(bucketName)
     print(imageName)
+    print(tableName)
+    
+    rekognitionClient = boto3.client('rekognition')
+    response = rekognitionClient.detect_labels(Image={'S3Object':{'Bucket':bucketName,'Name':imageName}}, MaxLabels=5)
+    labels = response["Labels"]
+    
+    for label in labels:
+        print(type(label))
+        print(label)
+        # del label["Instances"]
+        # del label["Parents"]
+    
+    # table = boto3.resource("dynamodb").Table(tableName)
+    # table.put_item(Item={"image": imageName, "results": "testResults"})
     
     print("... End of lambda_label_detection.py")
-
-    # rekognitionClient = boto3.client("rekognition")
 
     return {"statusCode": 200}
